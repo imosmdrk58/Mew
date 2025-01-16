637eeb3b5837caaa249cee10eb47ae31e85cbf20
Mustafa Cetin <115427747+mustafa-cetin@users.noreply.github.com>
2025-01-16 20:31:04 +0300

    is admin false

--- frontend/mangam/src/app/api/auth/register/route.ts
+++ frontend/mangam/src/app/api/auth/register/route.ts
@@ -36,7 +36,7 @@ export async function POST(request: NextRequest) {
         username,
         email,
         password_hash: hashedPassword,
-        is_admin: true, // todo: admin atamak için kendinizi register yaparken bunu yorumdan kaldırın
+        is_admin: false, // todo: admin atamak için kendinizi register yaparken bunu yorumdan kaldırın
       }),
     });
 
